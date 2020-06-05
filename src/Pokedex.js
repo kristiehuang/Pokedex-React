1da530d4c0c0d778299c36d85e976bb45f6fd7be
Kristie Huang <kristiehuang@berkeley.edu>
2020-06-04 21:21:02 -0700

    add mock data to page

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     AppBar, Toolbar, Grid,
-    Card, CardMedia, CardContent
+    Card, CardMedia, CardContent, CircularProgress, Typography
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import mockData from "./mockData";
 
 const useStyles = makeStyles({
     pokedexContainer: {
@@ -11,32 +12,74 @@ const useStyles = makeStyles({
         paddingLeft: "50px",
         paddingRight: "50px",
     },
+    cardMedia: {
+        margin: "auto",
+    },
+    cardContent: {
+        textAlign: "center",
+    },
 });
 
-const getPokemonCard = () => {
-    return (
-        <Grid item xs={12} sm={4}> {/* responsive - sm for smaller mobile screen sizes */}
-            <Card>
-                <CardContent>hi</CardContent>
-            </Card>
-        </Grid>
-    );
-};
+
+function toFirstCharUpper(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 
 const Pokedex = () => {
     const classes = useStyles();
+    const [pokemonData, setPokemonData] = useState(mockData);
+    // const getPokemonCard = (pokemonId) => {
+    //     console.log(pokemonData[`${pokemonId}`]);
+    //     const { id, name } = pokemonData[`${pokemonId}`];
+    //     const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    //     return (
+    //         <Grid item xs={12} sm={4} key={pokemonId}> {/* responsive - sm for smaller mobile screen sizes */}
+    //             <Card>
+    //                 <CardMedia
+    //                     className={classes.cardMedia}
+    //                     image={sprite}
+    //                     style={{ width: "130px", height: "130px" }}
+    //                 />
+    //                 <CardContent className={classes.cardContent}>
+    //                     <Typography>{`${id}. ${toFirstCharUpper(name)}`}</Typography>
+    //                 </CardContent>
+    //             </Card>
+    //         </Grid>
+    //     );
+    // };
+    function getPokemonCard(pokemonId) {
+        console.log(pokemonData[`${pokemonId}`]);
+        const { id, name } = pokemonData[`${pokemonId}`];
+        const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+        return (
+            <Grid item xs={12} sm={4} key={pokemonId}> {/* responsive - sm for smaller mobile screen sizes */}
+                <Card>
+                    <CardMedia
+                        className={classes.cardMedia}
+                        image={sprite}
+                        style={{ width: "130px", height: "130px" }}
+                    />
+                    <CardContent className={classes.cardContent}>
+                        <Typography>{`${id}. ${toFirstCharUpper(name)}`}</Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        );
+    }
+
     return (
         <>
             <AppBar position="static">
-                <Toolbar/>
+                <Toolbar />
             </AppBar>
-            <Grid container spacing={2} className={classes.pokedexContainer}>
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-
-            </Grid>
+            {pokemonData ? (
+                <Grid container spacing={2} className={classes.pokedexContainer}>
+                    {Object.keys(pokemonData).map((pokemonId) => getPokemonCard(pokemonId))}
+                </Grid>
+            ) : (
+                    <CircularProgress />
+                )}
 
         </>
     )
