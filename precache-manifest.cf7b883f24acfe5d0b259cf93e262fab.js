cbc4a4cbc084627cba23c34ee1a213cbb68dd5f8
Kristie Huang <kristiehuang01@gmail.com>
2020-06-07 00:38:04 -0700

    Updates

---
+++ precache-manifest.cf7b883f24acfe5d0b259cf93e262fab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369765be3021130f915c912dc1792472",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "0730ba963957b018cccc",
+    "url": "/pokedex-react/static/js/2.0d0f4fda.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex-react/static/js/2.0d0f4fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8717dd8f4a6cc3d475e1",
+    "url": "/pokedex-react/static/js/main.ec03e372.chunk.js"
+  },
+  {
+    "revision": "e0a3e8271c05ad0443ba",
+    "url": "/pokedex-react/static/js/runtime-main.b303f05e.js"
+  }
+]);
